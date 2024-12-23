43d03553518bd8619dc3cc02fa0211a7eed2dbbb
Khosbayar707 <torutora0415@gmail.com>
2024-12-23 23:12:27 +0800

    footer

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,3 +1,49 @@
+import { Mail } from "lucide-react";
+import { Phone } from "lucide-react";
+import { Roboto } from "next/font/google";
+const roboto = Roboto({ subsets: ["latin"], weight: "400" });
+
 export function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className={`${roboto.className}`}>
+      <div className="w-full h-[280px] bg-indigo-700 box-border p-[40px] text-white flex justify-around">
+        <div>
+          <img
+            src="/assets/Logo.png"
+            alt="logo"
+            className="w-[92px] h-[20px]"
+          />
+          <p>© 2024 Movie Z. All Rights Reserved.</p>
+        </div>
+        <div className="flex gap-8">
+          <div>
+            <p>Contact Information</p>
+            <div>
+              <div className="flex gap-3">
+                <Mail />
+                <p>Email:</p>
+              </div>
+
+              <p>support@movieZ.com</p>
+            </div>
+            <div>
+              <div className="flex gap-3">
+                <Phone />
+                <p>Phone:</p>
+              </div>
+
+              <p>+976 (11) 123-4567</p>
+            </div>
+          </div>
+          <div className="flex gap-5">
+            Follow us
+            <p>Facebook</p>
+            <p>Instagram</p>
+            <p>Twitter</p>
+            <p>Youtube</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+"use client";
 import Image from "next/image";
-import { Button } from "@/components/ui/button";
+import { Roboto } from "next/font/google";
+const roboto = Roboto({ subsets: ["latin"], weight: "400" });
+
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
-  );
+  return <div className={`${roboto.className}`}>hi, page</div>;
 }
